833bda1d5bcc92875220ebacd07d2c89d8b708d1
Peter Martin <peter_martin@sil.org>
2020-08-28 16:24:24 +0100

    [ABS-2187] Modify rebuild_comps.sh to build composites for Light, Regular and Black masters.

--- tools/rebuild_comps.sh
+++ tools/rebuild_comps.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 COMP_DEFS=composites.txt
-SOURCE_UFO=Lateef-Regular.ufo
-TARGET_UFO=Lateef-Regular-composites.ufo
-MASTER_DIR=source/masters
+# MASTER_DIR=source/masters
 
 echo
 echo "Rebuilding composites..."
 
-# Check we're in the same directory as the source UFO
-if [ ! -d "$SOURCE_UFO" ]
-then
-	echo "Error: Please run this script from the source directory containg '$SOURCE_UFO'"
-	exit
-fi
 
-psfbuildcomp \
-	-p scrlevel=w \
-	--noflatten \
-	--color \
-	--remove '_?(above|aboveLeft|below|center|ring|through)$' \
-	--preserve 'dia[AB]|alef$' \
-	-i "$COMP_DEFS" "$SOURCE_UFO" "$TARGET_UFO"
+build_one() {
+	
+	SOURCE_UFO=$1
+	TARGET_UFO=$1
+
+	# Check we're in the same directory as the source UFO
+	if [ ! -d "$SOURCE_UFO" ]
+	then
+		echo "Error: Please run this script from the source directory containing '$SOURCE_UFO'"
+		exit
+	fi
+
+	echo "Building composites from $SOURCE_UFO to $TARGET_UFO..."
+	psfbuildcomp \
+		-p scrlevel=w \
+		--noflatten \
+		--color \
+		--remove '_?(above|aboveLeft|below|center|ring|through)$' \
+		--preserve 'dia[AB]|alef$' \
+		-i "$COMP_DEFS" "$SOURCE_UFO" "$TARGET_UFO"
+	echo
+
+}
+
+# Build each master
+build_one "Lateef-Light.ufo"   "Lateef-Light-composites.ufo"
+build_one "Lateef-Regular.ufo" "Lateef-Regular-composites.ufo"
+build_one "Lateef-Black.ufo"   "Lateef-Black-composites.ufo"
 
 echo "Composite rebuild done."
