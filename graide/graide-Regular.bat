dd5d1865badb52da872dadada51390412cd2362e
Bob Hallissy <bobh0303@users.noreply.github.com>
2021-05-25 16:57:27 -0600

    use the interpolated (rather than master) UFO to build ttf for use by graide

--- graide/graide-Regular.bat
+++ graide/graide-Regular.bat
@@ -6,7 +6,8 @@ set ver=0_8_80
 if not exist ..\results\generated\Lateef-Regular.xml (
 	echo "file ..\results\generated\Lateef-Regular.xml is missing ... please build font first."
 ) else (
-	if not exist ..\results\Lateef-Regular-graide.ttf psfufo2ttf ..\source\masters\Lateef-Regular.ufo ..\results\Lateef-Regular-graide.ttf
+    rem build was done... now create the TTF we'll use:
+	if not exist ..\results\Lateef-Regular-graide.ttf psfufo2ttf ..\results\source\instances\Lateef-Regular.ufo ..\results\Lateef-Regular-graide.ttf
 	py -3 C:\SRC\GitHub\graide\graide -p Lateef-Regular.cfg
 	@rem graide%ver%_console.exe -p Lateef-Regular.cfg 
 )
