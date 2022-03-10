289f507ee44d82a687cb3c6851d607d0b0435aa5
Peter Martin <peter_martin@sil.org>
2022-03-10 15:35:51 +0000

    Add shell script to sync kerning from Regular to Light and Black masters.

---
+++ tools/copy_kerning_to_Light_and_Black.sh
@@ -0,0 +1,9 @@
+
+# Copy kerning data from Regular to Light and Black masters
+# kerning.plist
+cp source/masters/Lateef-Regular.ufo/kerning.plist source/masters/Lateef-Light.ufo/kerning.plist
+cp source/masters/Lateef-Regular.ufo/kerning.plist source/masters/Lateef-Black.ufo/kerning.plist
+
+# groups.plist
+cp source/masters/Lateef-Regular.ufo/groups.plist source/masters/Lateef-Light.ufo/groups.plist
+cp source/masters/Lateef-Regular.ufo/groups.plist source/masters/Lateef-Black.ufo/groups.plist
