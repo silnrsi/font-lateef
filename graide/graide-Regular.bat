a1a3467f7b1e71e1a127080f075c43bd808758ac
BobH <bob_hallissy@sil.org>
2022-06-13 18:14:09 -0600

    change graide execution path to use Python virtual environment.

--- graide/graide-Regular.bat
+++ graide/graide-Regular.bat
@@ -7,6 +7,7 @@ if not exist ..\results\generated\Lateef-Regular.xml (
 	echo "file ..\results\generated\Lateef-Regular.xml is missing ... please build font first."
 ) else (
 	if not exist ..\results\Lateef-Regular-graide.ttf psfufo2ttf ..\source\masters\Lateef-Regular.ufo ..\results\Lateef-Regular-graide.ttf
-	py -3 C:\SRC\GitHub\graide\graide -p Lateef-Regular.cfg
+	\DosUtils\graide-venv\Scripts\python.exe  \DosUtils\graide-venv\Scripts\graide -p Lateef-Regular.cfg
+	@ py -3 C:\SRC\GitHub\graide\graide -p Lateef-Regular.cfg
 	@rem graide%ver%_console.exe -p Lateef-Regular.cfg 
 )
